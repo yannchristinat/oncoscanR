48af1b0a4ff4705f1e66f1fc714faa09be4441eb
Yann Christinat <yann.christinat@hcuge.ch>
2022-03-08 17:21:32 +0100

    Updated .gitignore and vignette

---
+++ vignettes/oncoscanR.R
@@ -0,0 +1,13 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  if (!requireNamespace("BiocManager", quietly = TRUE))
+#      install.packages("BiocManager")
+#  BiocManager::install("GenomicRanges")
+#  
+#  install.packages(c("magrittr", "jsonlite", "readr"))
+
